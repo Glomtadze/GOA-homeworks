04582b73f1f10b6348c2144c22608f4f4e91686b
Glomtadze <glomtadze716@gmail.com>
2026-02-04 22:07:42 +0400

    day1

---
+++ day 1/lesson2.py
@@ -0,0 +1,32 @@
+#string
+print("hello")
+#integer
+print(7)
+#float
+print(1.5)
+#variable
+item="phone"
+print(item)
+
+name="giorgi"
+
+age=15
+
+adress="kutaisi"
+
+height=1.67
+
+weight=75
+
+print(name)
+
+print(age)
+
+print(adress)
+
+print(height)
+
+print(weight)
+
+friend1="Beka Meskhia"
+friend2="Zuka Katsarava"
