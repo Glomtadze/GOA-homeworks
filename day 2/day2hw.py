1fc1736f0b01f2cbd1dc4338eb0a2e07af1d2cd8
Glomtadze <glomtadze716@gmail.com>
2026-02-06 21:10:33 +0400

    day 2

---
+++ day 2/day2hw.py
@@ -0,0 +1,38 @@
+#declaration-დეკლარაცია არის მინიჭების ოპერატორი  
+#initialization-ინიციალიზება არის მნიშვნელობის მინიჭება ცვლადისთვის 
+#reinitialization-რეინიცილიზაცია არის ახალი მნიშვნელობის მინიჭება იგივე ცვლადისთვის
+#concatenation-კონკატენაცია არის სტრინგების შეერთება 
+#debugging-ნიშნავს კოდში შეცდომების გასწორებას
+
+number1=15
+
+number2=10
+print(number1+number2)
+
+print(number1-number2)
+
+print(number1*number2)
+
+print(number1/number2)
+
+print(number1//number2)
+
+print(number1%number2)
+
+number1=5
+number2=5
+
+print(number1**number2)
+
+birth_year=2010
+current_year=2026
+age=current_year-birth_year
+print(age)
+
+number1=5
+number2=4
+number3=6
+number4=9
+number5=10
+
+print((number1+number2+number3+number4+number5)/5)
