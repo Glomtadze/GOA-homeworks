4692a20eb903976ad066e56111bcfb5e29db95e9
Glomtadze <glomtadze716@gmail.com>
2026-02-11 17:01:03 +0400

    day 2

---
+++ day 2/lvl2hw.py
@@ -0,0 +1,34 @@
+number1=12
+
+number2=3
+print(number1+number2)
+
+print(number1-number2)
+
+print(number1*number2)
+
+print(number1/number2)
+
+print(number1//number2)
+
+print(number1%number2)
+
+print(number1**number2)
+
+birth_year=2010
+current_year=2026
+age=current_year-birth_year
+print(age)
+
+number1=3
+number2=4
+number3=5
+number4=7
+number5=9
+print((number1+number2+number3+number4+number5)/5)
+
+#declaration-დეკლარაცია არის მინიჭების ოპერატორი initialization-არის მნიშვნელობის მინიჭება ცვლადისთვის
+#reinitialization-ნიშნავს ცვლადისთვის ახალი მნიშვნელობის მინიჭებას
+#concatenation-კონკატენაცია არის სტრინგების შეერთება
+#debugging-ნიშნავს კოდში შეცდომის გამოსწორებას
+
