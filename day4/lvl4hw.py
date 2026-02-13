0651f5307f35802512de918409f598f4aa17dcbd
Glomtadze <glomtadze716@gmail.com>
2026-02-13 21:28:48 +0400

    day4

---
+++ day4/lvl4hw.py
@@ -0,0 +1,17 @@
+#snake_case-არის წერის სტილი რომელიც გულისხმობს რომ თუ ცვლადის სახელისთვის გვიწევს 1 ზე მეტი სიტყვის გამოყენება ყველა სიტყვა უნდა გამოვყოთ ქვედა ტირის გამოყენებით
+#case sensitive language-ნიშნავს რომ პითონისთვის დიდი შრიფტით დაწერილი ასოები განსხვავდება პატარა შრიფტით დაწერილი ასოებისგან ანუ A პითონისთვის სრულიად განსხვავდება a-სგან
+#input-ის მეშვეობით შეგვიძლია პროგრამაში შევიყვანოთ ინფორმაცია ან მომხმარებელს მივცეთ იმის საშუალება რომ ინფორმაცია შეიყვანოს პროგრამაში
+#output-არის ინფორმაცია რომელსაც პროგრამა გვიბრუნებს
+
+
+user_name=input("ENTER YOUR NAME: ")
+
+user_surename=input("ENTER YOUR SURENAME: ")
+
+user_adress=input("ENTER YOUR ADRESS: ")
+
+user_hobby=input("ENTER YOUR HOBBY: ")
+
+fav_movie=input("ENTER YOUR FAVOURITE MOVIE NAME: ")
+
+print("HELLO MY NAME IS "+user_name+" "+"MY SURENAME IS"+" "+user_surename+" "+"I LIVE IN "+user_adress+" "+"MY HOBBY IS "+user_hobby+" "+"AND MY FAVOURITE MOVIE IS "+fav_movie)
