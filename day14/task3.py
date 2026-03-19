cf7044b52442974ccd5273356786bbacb9ac379e
Glomtadze <glomtadze716@gmail.com>
2026-03-20 01:27:36 +0400

    day14

---
+++ day14/task3.py
@@ -0,0 +1,3 @@
+#task4
+for i in range(25, 150, 4):
+    print(i)
---
+++ day14/task4.py
@@ -0,0 +1,8 @@
+#task5
+for i in range(100, 1000, 10):
+    print(i)
+
+#task6
+for i in range(100):
+    print("პითონი მაგარია")
+
