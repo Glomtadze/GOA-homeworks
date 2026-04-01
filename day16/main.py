f31f48e3c8101dceb7c372874b1149af59bcd42f
Glomtadze <glomtadze716@gmail.com>
2026-04-01 22:33:10 +0400

    day17

--- day16/main.py
+++ day16/main.py
@@ -1,30 +1,31 @@
-#task1
-for i in range(100):
-    print(i)
+# #task1
+# for i in range(100):
+#     print(i)
 
-#task2
-for i in range(40,252,2):
-    print(i)
+# #task2
+# for i in range(40,252,2):
+#     print(i)
 
-#task3
-for i in range(200,400,5):
-    print(i)
+# #task3
+# for i in range(200,400,5):
+#     print(i)
 
-for i in range(200,400,4):
-    print(i)
-#task4
-number=int(input("enter any number: "))
-i=2
-while i<=number :
-    print(i)
-    i=i+1
+# for i in range(200,400,4):
+#     print(i)
+# #task4
+# number=int(input("enter any number: "))
+# i=2
+# while i<=number :
+#     print(i)
+#     i=i+1
 
 # #task5
-number=int(input("enter any number: "))
-i=0
-while i<number:
-     print(number)
-     number=number-1
+# number=int(input("enter any number: "))
+# while number>0:
+#     print(number)
+#     number=number-1
+    
+
 
 #task6
 number=35
@@ -35,4 +36,3 @@ while True:
         print("correct")
     else:
         print("try again")
-    
