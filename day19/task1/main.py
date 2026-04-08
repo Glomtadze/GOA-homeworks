f08204618e6e554f127cac5b32ed4666cde48787
Glomtadze <glomtadze716@gmail.com>
2026-04-08 21:47:02 +0400

    day19

---
+++ day19/task1/main.py
@@ -0,0 +1,9 @@
+for i in range(100,400):
+    if i%5==0 and i%3!=0:
+        print(i)
+
+i=100
+while i<400:
+    if i%5==0 and i%3!=0:
+        print(i)
+    i=i+1
---
+++ day19/task6/main.py
@@ -0,0 +1,11 @@
+count=0
+i=int(input("enter any number: "))
+while i>=0:
+    if i>=0:
+        count=count+1
+    i=int(input("enter any number: "))
+if i<0:
+    print(count)
+
+   
+
