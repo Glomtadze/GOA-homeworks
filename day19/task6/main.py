1fa4580e369f0b471be5e14c5c5e6831d0958af5
Glomtadze <glomtadze716@gmail.com>
2026-04-09 15:36:56 +0400

    day19

--- day19/task6/main.py
+++ day19/task6/main.py
@@ -2,7 +2,7 @@ count=0
 i=int(input("enter any number: "))
 while i>=0:
     if i>=0:
-        count=count+1
+        count=count+i
     i=int(input("enter any number: "))
 if i<0:
     print(count)
